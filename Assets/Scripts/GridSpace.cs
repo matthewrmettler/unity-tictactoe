f99afa47f1193e991ee6003214b773030e9dc128
Matthew Mettler <matthewrmettler@gmail.com>
2016-10-24 16:56:47 -0400

    Finished 'Foundational Gameplay'

---
+++ Assets/Scripts/GridSpace.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GridSpace : MonoBehaviour {
+	public Button button;
+	public Text buttonText;
+	public string playerSide;
+
+	public void SetSpace() 
+	{
+		buttonText.text = playerSide;
+		button.interactable = false;
+	}
+
+}
