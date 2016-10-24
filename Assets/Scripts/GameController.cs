657fabc2a80c9578a465d98577427ead723d7609
Matthew Mettler <matthewrmettler@gmail.com>
2016-10-24 17:39:03 -0400

    Finished 'win conditions and taking turns'

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,6 +4,7 @@ using System.Collections;
 
 public class GameController : MonoBehaviour {
 	public Text[] buttonList;
+	private string playerSide;
 
 	/**
 	 * This gets called when the game starts.
@@ -11,6 +12,7 @@ public class GameController : MonoBehaviour {
 	void Awake()
 	{
 		SetGameControllerReferenceOnButtons ();
+		playerSide = "X";
 	}
 
 	void SetGameControllerReferenceOnButtons ()
@@ -23,11 +25,74 @@ public class GameController : MonoBehaviour {
 
 	public string GetPlayerSide() 
 	{
-		return "?";
+		return playerSide;
 	}
 
 	public void EndTurn()
 	{
-		Debug.Log("EndTurn is not implemented!");
+		//Top row
+		if (buttonList [0].text == playerSide && buttonList [1].text == playerSide && buttonList [2].text == playerSide)
+		{
+			GameOver();
+		}
+
+		//Middle row
+		if (buttonList [3].text == playerSide && buttonList [4].text == playerSide && buttonList [5].text == playerSide)
+		{
+			GameOver();
+		}
+
+		//Bottom row
+		if (buttonList [6].text == playerSide && buttonList [7].text == playerSide && buttonList [8].text == playerSide)
+		{
+			GameOver();
+		}
+
+		//Left column
+		if (buttonList [0].text == playerSide && buttonList [3].text == playerSide && buttonList [6].text == playerSide)
+		{
+			GameOver();
+		}
+
+		//Middle column
+		if (buttonList [1].text == playerSide && buttonList [4].text == playerSide && buttonList [7].text == playerSide)
+		{
+			GameOver();
+		}
+
+		//Right column
+		if (buttonList [2].text == playerSide && buttonList [5].text == playerSide && buttonList [8].text == playerSide)
+		{
+			GameOver();
+		}
+
+		//Top left to bottom right diagonal
+		if (buttonList [0].text == playerSide && buttonList [4].text == playerSide && buttonList [8].text == playerSide)
+		{
+			GameOver();
+		}
+
+		//Bottom left to top right diagonal
+		if (buttonList [2].text == playerSide && buttonList [4].text == playerSide && buttonList [6].text == playerSide)
+		{
+			GameOver();
+		}
+
+		ChangeSides();
+	}
+
+	void GameOver() 
+	{
+		//Disable all the buttons
+		for (int i = 0; i < buttonList.Length; i++)
+		{
+			buttonList[i].GetComponentInParent<Button>().interactable = false;
+		}
+		Debug.Log ("Game over!");
+	}
+
+	void ChangeSides()
+	{
+		playerSide = (playerSide == "X") ? "O" : "X";
 	}
 }
