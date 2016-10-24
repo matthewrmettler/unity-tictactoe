dd7b37274c99f5a2c369494cb562921f5846e852
Matthew Mettler <matthewrmettler@gmail.com>
2016-10-24 17:31:54 -0400

    Finished 'controlling the game'

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+	public Text[] buttonList;
+
+	/**
+	 * This gets called when the game starts.
+	 **/
+	void Awake()
+	{
+		SetGameControllerReferenceOnButtons ();
+	}
+
+	void SetGameControllerReferenceOnButtons ()
+	{
+		for (int i = 0; i < buttonList.Length; i++) 
+		{
+			buttonList [i].GetComponentInParent<GridSpace> ().SetGameControllerReference(this);
+		}
+	}
+
+	public string GetPlayerSide() 
+	{
+		return "?";
+	}
+
+	public void EndTurn()
+	{
+		Debug.Log("EndTurn is not implemented!");
+	}
+}
