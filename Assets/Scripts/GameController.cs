bf93536982eacbee031148aeb72700dc091cb546
Matthew Mettler <matthewrmettler@gmail.com>
2016-10-24 18:09:32 -0400

    Restarting the game

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,6 +7,7 @@ public class GameController : MonoBehaviour {
 	private string playerSide;
 
 	public GameObject gameOverPanel;
+	public GameObject restartButton;
 	public Text gameOverText;
 
 	//Count the number of moves (to figure out when it's a tie/draw)
@@ -17,9 +18,7 @@ public class GameController : MonoBehaviour {
 	void Awake()
 	{
 		SetGameControllerReferenceOnButtons ();
-		playerSide = "X";
-		gameOverPanel.SetActive(false);
-		moveCount = 0;
+		RestartGame ();
 	}
 
 	void SetGameControllerReferenceOnButtons ()
@@ -42,7 +41,7 @@ public class GameController : MonoBehaviour {
 		//Check for end of game
 		if (moveCount >= 9) 
 		{
-			SetGameOverText ("It's a draw!");
+			GameOver ("draw");
 		}
 		//Otherwise, keep going
 		ChangeSides();
@@ -67,19 +66,21 @@ public class GameController : MonoBehaviour {
 		//Bottom left to top right diagonal
 		|| (buttonList [2].text == playerSide && buttonList [4].text == playerSide && buttonList [6].text == playerSide))
 		{
-			GameOver();
+			GameOver(playerSide);
 		}
 	}
 
-	void GameOver() 
+	void GameOver(string winningPlayer) 
 	{
 		//Disable all the buttons
-		for (int i = 0; i < buttonList.Length; i++)
-		{
-			buttonList[i].GetComponentInParent<Button>().interactable = false;
-		}
+		SetBoardInteractable(false);
 		gameOverPanel.SetActive(true);
-		SetGameOverText(playerSide + " Wins!");
+		restartButton.SetActive (true);
+		if (winningPlayer == "draw") {
+			SetGameOverText("It's a draw!");
+		} else {
+			SetGameOverText(winningPlayer + " Wins!");
+		}
 		Debug.Log ("Game over!");
 	}
 
@@ -92,5 +93,24 @@ public class GameController : MonoBehaviour {
 	{
 		gameOverPanel.SetActive(true);
 		gameOverText.text = value;
+	}
+
+	public void RestartGame()
+	{
+		playerSide = "X";
+		moveCount = 0;
+		gameOverPanel.SetActive(false);
+		restartButton.SetActive (false);
+		//Enable all the buttons
+		SetBoardInteractable(true);
+	}
+
+	public void SetBoardInteractable(bool toggle)
+	{
+		for (int i = 0; i < buttonList.Length; i++)
+		{
+			buttonList[i].GetComponentInParent<Button>().interactable = toggle;
+			if (toggle) buttonList [i].text = "";
+		}
 	}
 }
